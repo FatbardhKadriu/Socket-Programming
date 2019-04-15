3ac7d381183586db5e6793a70f8d5876bd9abc72
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-04-15 19:12:53 +0200

    Update Serveri.py

--- FIEK-TCP/Serveri/Serveri/Serveri.py
+++ FIEK-TCP/Serveri/Serveri/Serveri.py
@@ -1,1 +1,24 @@
+from socket import *
+from _thread import *
+import math 
+import random 
+
+serverName = 'localhost'
+serverPort = 12000 
+serverAddress = (serverName, serverPort)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+try:
+    serverSocket.bind(serverAddress)
+except socket.error:
+    print("Nuk u arrit lidhja me klientin")
+    sys.exit()
+
+serverSocket.listen(5)
+
+print("**********************************************************************")
+print("Projekti i parë në lëndën Rrjeta Kompjuterike\n")
+print("\t\t\tPROGRAMIMI ME SOCKET-A")
+print("\t\tFIEK - TCP Serveri\n\tFatbardh Kadriu")
+print("\nServeri është në pritje të lidhjeve")
+print("\n**********************************************************************")
 
